2320098561a354d0fc6e1bf349a676558ce06b90
usc-KWM007 <KWM007@student.usc.edu.au>
2025-01-17 12:56:23 +1000

    Added bg-image
    Added bg-image for contact page, info coming soon

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,12 @@
 <x-layout>
-    <section class="my-10 text-center font-bold border-b">
+    <section class="text-center font-bold border-b">
         <h1 class="text-4xl">Contact Us</h1>
     </section>
+    <article>
+    <img class="opacity-30 absolute h-full w-full" src={{ Vite::asset('resources/images/gallery3.jpg') }} alt="background image" />
+    
+    <div class="">
+        <p>Contact info</p>
+    </div>
+</article>
 </x-layout>
